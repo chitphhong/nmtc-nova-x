d9366cefaff9b6fe694ca7acbfa11558534b06eb
ChitphongEiEi <kruyminezz@gmail.com>
2026-09-19 21:11:49 +0700

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,10 +13,10 @@ export default function HomePage() {
     <>
       <HeroSection />
       <MaterialPassport />
-      <ModularBreakdown />
+      {/* <ModularBreakdown /> */}
       <GrandSponsor />
       <WisahakijPartner />
-      <TeamShowcase />
+      {/* <TeamShowcase /> */}
     </>
   );
 }
